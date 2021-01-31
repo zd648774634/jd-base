8bc28aa9d9bd6f190050edc29110881b5901acce
zd648774634 <lianhu7847@126.com>
2021-01-31 10:13:44 +0800

    Update rm_log.sh

--- rm_log.sh
+++ rm_log.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ## Author: Evine Deng
-## Source: https://github.com/EvineDeng/jd-base
+## Source: https://github.com/zd648774634/jd-base
 ## Modified： 2021-01-21
 ## Version： v3.2.4
 
